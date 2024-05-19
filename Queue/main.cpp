93b267408d7dca89d387b276390681c68ec4add0
IvanRulevskyi <rulevskiy4@gmail.com>
2024-05-19 22:59:36 +0300

    Finish

--- Queue/main.cpp
+++ Queue/main.cpp
@@ -1,14 +1,59 @@
-//
-//  main.cpp
-//  Queue
-//
-//  Created by Иван Рулевский on 19.05.2024.
-//
-
 #include <iostream>
+#include "queue"
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+class Event
+{
+private:
+    string eventName;
+public:
+    Event(const std::string& name) : eventName(name)
+    { }
+    std::string getName() const
+    {
+        return eventName;
+    }
+};
+
+class EventProcessor
+{
+public:
+    void processEvent(const Event& event)
+    {
+        cout << "Processing event: " << event.getName() << endl;
+    }
+};
+
+
+int main()
+{
+    queue<queue<Event>> outerQueue;
+    
+    queue<Event> queue1;
+    queue1.push(Event("Event1.1"));
+    queue1.push(Event("Event1.2"));
+    
+    queue<Event> queue2;
+    queue1.push(Event("Event2.1"));
+    queue1.push(Event("Event2.2"));
+    
+    queue<Event> queue3;
+    queue1.push(Event("Event3.1"));
+    queue1.push(Event("Event3.2"));
+    
+    EventProcessor processor;
+
+    while (!outerQueue.empty()) {
+            queue<Event>& currentQueue = outerQueue.front();
+            
+            while (!currentQueue.empty()) {
+                Event currentEvent = currentQueue.front();
+                processor.processEvent(currentEvent);
+                currentQueue.pop();
+            }
+
+            outerQueue.pop();
+        }
+    
+    
 }
